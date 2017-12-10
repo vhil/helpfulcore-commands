19c4e0e48410d529135ced4f5304b0a33d2ad2b2
Volodymyr Hil <vhi@cabana.dk>
2017-12-10 22:58:36 +0100

    implement duplicate item replacements

---
+++ src/Helpfulcore.Commands/Pipelines/UiDuplicateItem/BucketsItemDuplicate.cs
@@ -0,0 +1,22 @@
+﻿namespace Helpfulcore.Commands.Pipelines.UiDuplicateItem
+{
+    using Sitecore.Buckets.Pipelines.UI;
+    using System.Reflection;
+    using Sitecore.Web.UI.Sheer;
+
+    public class BucketsItemDuplicate : ItemDuplicate
+    {
+        public new void Execute(ClientPipelineArgs args)
+        {
+            base.Execute(args);
+            Resume(args);
+        }
+
+        private static void Resume(ClientPipelineArgs args)
+        {
+            const BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+            var method = args.GetType().GetMethod("Resume", bindFlags);
+            method?.Invoke(args, new object[0]);
+        }
+    }
+}
--- src/Helpfulcore.Commands/Pipelines/UiDuplicateItem/UpdateDataSources.cs
+++ src/Helpfulcore.Commands/Pipelines/UiDuplicateItem/UpdateDataSources.cs
@@ -1,12 +1,28 @@
-﻿using Sitecore.Web.UI.Sheer;
-
-namespace Helpfulcore.Commands.Pipelines.UiDuplicateItem
+﻿namespace Helpfulcore.Commands.Pipelines.UiDuplicateItem
 {
+    using Sitecore.Data;
+    using Sitecore.Data.Managers;
+    using Sitecore.Web.UI.Sheer;
+
     public class UpdateDataSources
     {
+        private readonly DataSourceService service;
+
+        public UpdateDataSources()
+        {
+            this.service = new DataSourceService();
+        }
+
         public void Execute(ClientPipelineArgs args)
         {
-            var parameters = args.Parameters;
+            var database = Database.GetDatabase(args.Parameters["database"]);
+            var language = LanguageManager.GetLanguage(args.Parameters["language"]);
+            var originalItem = database.GetItem(new ID(args.Parameters["id"]), language);
+            var duplicatedItem = database.GetItem(originalItem.Parent.Paths.FullPath + "/" + args.Parameters["name"], language);
+
+            service.UpdateRelativeDataSourses(originalItem, duplicatedItem);
+
+            args.AbortPipeline();
         }
     }
 }
